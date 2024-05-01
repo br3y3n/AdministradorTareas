2b0a256f733879e6dabd3a79220d87ddcf746c5d
[brayan23] <[bdelgadomunoz@gmail.com]>
2024-04-30 22:27:32 -0500

    add dark mode

--- app/src/main/java/com/administradortareas/myapplication/MainActivity.java
+++ app/src/main/java/com/administradortareas/myapplication/MainActivity.java
@@ -1,133 +1,46 @@
 package com.administradortareas.myapplication;
 
-import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
-
-    private List<TaskCategory> categories;
-    private final List<Task> tasks = new ArrayList<>();
-
-
-    private RecyclerView rvCategories;
-    private CategoriesAdapter categoriesAdapter;
-
-    private RecyclerView rvTasks;
-    private TasksAdapter tasksAdapter;
-
-    private FloatingActionButton fabAddTask;
-
-
+    Switch switchMode;
+    boolean nightMode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initComponent();
-        initUI();
-        initListeners();
-    }
+        getSupportActionBar().hide();
+        switchMode = findViewById(R.id.switch_mode);
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMode = sharedPreferences.getBoolean("night", false);
 
-    private void initListeners() {
-        fabAddTask.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDialog();
+        if(nightMode){
+            switchMode.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        switchMode.setOnClickListener(view -> {
+            if(nightMode){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                editor = sharedPreferences.edit();
+                editor.putBoolean("night", false);
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                editor = sharedPreferences.edit();
+                editor.putBoolean("night", true);
             }
-        });
-    }
-
-    private void showDialog() {
-        final Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.dialog_task);
-
-        final Button btnAddTask = dialog.findViewById(R.id.btnAddTask);
-        final EditText etTask = dialog.findViewById(R.id.etTask);
-        final RadioGroup rgCategories = dialog.findViewById(R.id.rgCategories);
-
-        btnAddTask.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String currentTask = etTask.getText().toString().trim();
-                if (!currentTask.isEmpty()) {
-                    int selectedId = rgCategories.getCheckedRadioButtonId();
-                    RadioButton selectedRadioButton = dialog.findViewById(selectedId);
-                    TaskCategory currentCategory;
-                    String selectedCategoryString = selectedRadioButton.getText().toString();
-                    if (selectedCategoryString.equals(getString(R.string.todo_dialog_category_business))) {
-                        currentCategory = TaskCategory.Business;
-                    } else if (selectedCategoryString.equals(getString(R.string.todo_dialog_category_personal))) {
-                        currentCategory = TaskCategory.Personal;
-                    } else {
-                        currentCategory = TaskCategory.Other;
-                    }
-
-                    tasks.add(new Task(currentTask, currentCategory));
-                    updateTasks();
-                    dialog.dismiss();
-                }
-            }
+            editor.apply();
         });
 
-        dialog.show();
+
     }
 
-    private void initComponent() {
-        rvCategories = findViewById(R.id.rvCategories);
-        rvTasks = findViewById(R.id.rvTasks);
-        fabAddTask = findViewById(R.id.fabAddTask);
-    }
-
-    private void initUI() {
-        categoriesAdapter = new CategoriesAdapter(categories, position -> updateCategories(position));
-        rvCategories.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        rvCategories.setAdapter(categoriesAdapter);
-
-        tasksAdapter = new TasksAdapter(Collections.singletonList((Task) tasks),
-                (TasksAdapter.OnTaskSelectedListener) position -> onItemSelected(position));
-        rvTasks.setLayoutManager(new LinearLayoutManager(this));
-        rvTasks.setAdapter(tasksAdapter);
-    }
-
-    private void onItemSelected(int position) {
-        tasks.get(position).setSelected(!tasks.get(position).isSelected());
-        updateTasks();
-    }
-
-    private void updateCategories(int position) {
-        categories.get(position).setSelected(!categories.get(position).isSelected());
-        categoriesAdapter.notifyItemChanged(position);
-        updateTasks();
-    }
-
-    private void updateTasks() {
-        List<TaskCategory> selectedCategories;
-        selectedCategories = new ArrayList<>();
-        for (TaskCategory category : categories) {
-            if (category.isSelected()) {
-                selectedCategories.add(category);
-            }
-        }
-        ArrayList<Task> newTasks = new ArrayList<>();
-        for (Task task : tasks) {
-            if (selectedCategories.contains(task.getCategory())) {
-                newTasks.add(task);
-            }
-        }
-        tasksAdapter.notifyDataSetChanged();
-    }
 }
